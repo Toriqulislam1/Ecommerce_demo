e694ebd2b6001f722380792b1eadbffe403a449a
toriqul <toriqulislam2720@gmail.com>
2023-09-09 00:53:38 +0600

    search bar

--- app/Http/Controllers/frontend/searchController.php
+++ app/Http/Controllers/frontend/searchController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\color;
 use App\Models\product;
+use App\Models\size;
 use Illuminate\Http\Request;
 
 class searchController extends Controller
@@ -12,17 +14,26 @@ class searchController extends Controller
 
         $data = $request->all();
 
+        $colors = color::all();
+        $sizes = size::all();
+
         $products = product::where(function($q) use($data){
 
             if(!empty($data['q'])&& $data['q']!='' && $data['q']!='undefined'){
                 $q->where(function($q) use($data){
                     $q->where('product_name','like', '%'. $data['q'].'%');
+                    $q->OrWhere('short_description','like', '%'. $data['q'].'%');
+                    $q->OrWhere('long_description','like', '%'. $data['q'].'%');
                 });
             }
 
 
         })->get();
 
-        return view('frontend.search',['products'=>$products]);
+        return view('frontend.search',[
+            'products'=>$products,
+            'colors'=>$colors,
+            'sizes'=>$sizes,
+        ]);
     }
 }
